62eac503e1f72d60436ffc63dd4e2c383f6ddb68
acslayer02 <98080973+acslayer02@users.noreply.github.com>
2022-03-29 00:48:33 +0300

    main

---
+++ servlet web/candidates.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>EVOTE</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins&family=Spartan:wght@500&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="header.css" />
+    <link rel="stylesheet" href="homepage.css" />
+    <link rel="stylesheet" href="candidates.css" />
+  </head>
+  <body>
+    <header class="header">
+      <div class="headercontainer">
+        <div class="title">EVOTE</div>
+        <a href="main page.html"> <div class="nav-content">Home</div></a>
+        <a href="candidates.php"><div class="nav-content">Candidates</div></a>
+        <a href=""><div class="nav-content">Login</div></a>
+      </div>
+    </header>
+    <div class="container">
+      <div class="content">
+        <div class="candidate">
+          <div class="pic"></div>
+          <div class="profilname">chab l3arbi</div>
+        </div>
+        <div class="candidate">
+          <div class="pic"></div>
+          <div class="profilname">shikha trax</div>
+        </div>
+        <div class="candidate">
+          <div class="pic"></div>
+          <div class="profilname">adam lallana</div>
+        </div>
+      </div>
+    </div>

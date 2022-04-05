b734b085eb7ad7d706ae64043a37194aa1a926d1
YasserHajji <yasser.hajji@student.hamk.fi>
2022-04-06 00:19:06 +0300

    adding universal header and footer for all the pages

--- candidates.php
+++ candidates.php
@@ -1,42 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>EVOTE</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins&family=Spartan:wght@500&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="header.css" />
-    <link rel="stylesheet" href="homepage.css" />
-    <link rel="stylesheet" href="candidates.css" />
-  </head>
-  <body>
-    <header class="header">
-      <div class="headercontainer">
-        <div class="title">EVOTE</div>
-        <a href="main page.html"> <div class="nav-content">Home</div></a>
-        <a href="candidates.php"><div class="nav-content">Candidates</div></a>
-        <a href=""><div class="nav-content">Login</div></a>
-      </div>
-    </header>
-    <div class="container">
-      <div class="content">
-        <div class="candidate">
-          <div class="pic"></div>
-          <a href=""><div class="profilname">cheb l3arbi</a></div>
-        </div>
-        <div class="candidate">
-          <div class="pic"></div>
-          <a href=""><div class="profilname">shikha trax</a></div>
-        </div>
-        <div class="candidate">
-          <div class="pic"></div>
-          <a href=""><div class="profilname">zimbabwe</a></div>
-        </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>EVOTE</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Spartan:wght@500&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="homepage.css" />
+  <link rel="stylesheet" href="candidates.css" />
+</head>
+<?php
+include "header.php";
+?>
+
+<body>
+  <div class="container">
+    <div class="content">
+      <div class="candidate">
+        <div class="pic"></div>
+        <a href="">
+          <div class="profilname">cheb l3arbi
+        </a>
       </div>
     </div>
+    <div class="candidate">
+      <div class="pic"></div>
+      <a href="">
+        <div class="profilname">shikha trax
+      </a>
+    </div>
+  </div>
+  <div class="candidate">
+    <div class="pic"></div>
+    <a href="">
+      <div class="profilname">zimbabwe
+    </a>
+  </div>
+  </div>
+  </div>
+  </div>
+  <?php
+  include "footer.php";
+  ?>
